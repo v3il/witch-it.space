76625d10c5c4d3ced560ac34e3af04fb255f69c4
Dmitry <v3il@ukr.net>
2020-09-30 22:49:30 +0300

    generateTokenData helper

---
+++ api/util/generateTokenData.js
@@ -0,0 +1,13 @@
+import { sign } from 'jsonwebtoken'
+import { config } from '../config'
+
+export const generateTokenData = (userPublicData) => {
+    const tokenDuration = 60 * 60 * 24 * 7 // 7 days
+
+    const expires = Math.floor(Date.now() / 1000 + tokenDuration)
+    const token = sign(userPublicData, config.JWT_SECRET, {
+        expiresIn: tokenDuration
+    })
+
+    return { token, expires }
+}
