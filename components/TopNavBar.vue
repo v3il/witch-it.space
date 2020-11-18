f6f64f412cb49a10b7ef1be1eef93360da8f48ca
Dmitry <v3il@ukr.net>
2020-11-18 22:49:27 +0200

    Clear top nav bar component

--- components/TopNavBar.vue
+++ components/TopNavBar.vue
@@ -12,10 +12,6 @@
 </template>
 
 <script>
-// import { createNamespacedHelpers } from 'vuex'
-//
-// const { mapState, mapMutations } = createNamespacedHelpers('theme')
-
 import ThemeSwitcher from '@/components/ThemeSwitcher'
 import LocaleSwitcher from '@/components/LocaleSwitcher'
 
@@ -25,29 +21,6 @@ export default {
     components: {
         ThemeSwitcher,
         LocaleSwitcher
-    },
-
-    // computed: {
-    //     ...mapState(['theme'])
-    // },
-
-    methods: {
-        // ...mapMutations(['setTheme']),
-
-        // onSubmit () {
-        //
-        // },
-
-        // onC (theme) {
-        //     // const theme = target.checked ? 'dark' : 'light'
-        //     console.log(2, theme)
-        //     this.$emit('changeTheme', theme)
-        //
-        //     // window.document.body.classList.toggle('body--light', theme === 'light')
-        //     // window.document.body.classList.toggle('body--dark', theme === 'dark')
-        //
-        //     // this.setTheme(theme)
-        // }
     }
 }
 </script>
