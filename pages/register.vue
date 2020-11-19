4eff1157b747d5ab1cfcfcbfae8e15b9b8a5e61e
Dmitry <v3il@ukr.net>
2020-11-19 22:51:46 +0200

    Removed base components

--- pages/register.vue
+++ pages/register.vue
@@ -1,69 +1,10 @@
 <template>
-  <div class="wit-flex wit-flex--center login-page">
-    <div class="login-page__container">
-      <div class="login-page__form wit-background--content wit-transition wit-offset-bottom--xxlg">
-        <header class="login-page__form-header wit-background--primary wit-position--relative">
-          <h1 class="wit-color--white wit-text--center wit-font-weight--600 wit--font-size--sm wit-line-height--sm wit-font-family--secondary">
-            {{ $t('welcome_back') }}
-          </h1>
-          <p class="wit-color--white wit-text--center wit-offset-bottom--md wit-color--white-half">
-            {{ $t('sign_in_to_continue') }}
-          </p>
-
-          <div class="login-page__image-block wit-position--absolute wit-background--content wit-transition" />
-        </header>
-
-        <div class="login-page__form-content">
-          <form class="auth__form" @submit.prevent="onSubmit">
-            <Label for="login" class="wit-offset-bottom--xs">{{ $t('login') }}</Label>
-            <Input id="login" v-model="login" type="text" :placeholder="$t('enter_login')" class="wit-offset-bottom--sm" />
-
-            <Label for="password" class="wit-offset-bottom--xs">{{ $t('password') }}</Label>
-            <Input id="password" v-model="password" :placeholder="$t('enter_password')" type="password" class="wit-offset-bottom--md" />
-
-            <div class="wit-flex wit-flex--center">
-              <div class="wit-flex wit-flex--center">
-                <a href="/api/auth/steam" class="auth__social-link wit-offset-right--xs"><span class="mdi mdi-steam" /></a>
-                <a href="/api/auth/discord" class="auth__social-link wit-offset-right--xs"><span class="mdi mdi-discord" /></a>
-                <a href="/api/auth/google" class="auth__social-link wit-offset-right--xs"><span class="mdi mdi-google" /></a>
-              </div>
-
-              <Button type="submit" class="wit-offset-left--auto">
-                {{ $t('login_in') }}
-              </Button>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <p class="wit-text--center">
-        {{ $t('dont_have_account') }} <Link class="wit-color--primary wit-font-weight--500" to="/login">
-          {{ $t('sign_up') }} Login!
-        </Link>
-      </p>
-
-      <p class="wit-text--center">
-        ©{{ $options.year }}. {{ $t('crafted_by') }}
-      </p>
-    </div>
-  </div>
+  <div class="wit-flex wit-flex--center login-page" />
 </template>
 
 <script>
-import Label from '@/components/base/Label'
-import Input from '@/components/base/Input'
-import Button from '@/components/base/Button'
-import Link from '@/components/base/Link'
-
 export default {
     year: new Date().getFullYear(),
-
-    components: {
-        Label,
-        Input,
-        Button,
-        Link
-    },
 
     data: () => ({
         login: '',
