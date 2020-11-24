feb85ecc87449aedb07c15f9e0a050096c43da0c
Dmitry <v3il@ukr.net>
2020-11-24 16:21:35 +0200

    Main menu and pages

--- components/TopNavBar.vue
+++ components/TopNavBar.vue
@@ -1,5 +1,40 @@
 <template>
-  <header class="wit-transition wit-header">
+  <header class="wit-transition wit-header wit-flex wit-flex--justify-between">
+    <img class="login-page__image" src="images/hey.png" alt="Hey!">
+
+    <ul class="wit-flex wit-flex--align-center">
+      <li class="wit-offset-right--md">
+        <nuxt-link to="/" class="wit-flex wit-flex--align-center wit-color--white">
+          <!--          <b-icon size="is-small" class="is-size-5 wit-offset-right&#45;&#45;xs" icon="cog-sync" />-->
+          <span class="wit-inline-block">Мой маркет</span>
+        </nuxt-link>
+      </li>
+      <li class="wit-offset-right--md">
+        <nuxt-link to="/" class="wit-flex wit-flex--align-center wit-color--white">
+          <!--          <b-icon size="is-small" class="is-size-5 wit-offset-right&#45;&#45;xs" icon="cog-sync" />-->
+          <span class="wit-inline-block">Список желаний</span>
+        </nuxt-link>
+      </li>
+      <li class="wit-offset-right--md">
+        <nuxt-link to="/" class="wit-flex wit-flex--align-center wit-color--white">
+          <!--          <b-icon size="is-small" class="is-size-5 wit-offset-right&#45;&#45;xs" icon="cog-sync" />-->
+          <span class="wit-inline-block">Профили</span>
+        </nuxt-link>
+      </li>
+      <li class="wit-offset-right--md">
+        <nuxt-link to="/" class="wit-flex wit-flex--align-center wit-color--white">
+          <!--          <b-icon size="is-small" class="is-size-5 wit-offset-right&#45;&#45;xs" icon="cog-sync" />-->
+          <span class="wit-inline-block">Предметы</span>
+        </nuxt-link>
+      </li>
+      <li class="wit-offset-right--md">
+        <nuxt-link to="/" class="wit-flex wit-flex--align-center wit-color--white">
+          <!--          <b-icon size="is-small" class="is-size-5 wit-offset-right&#45;&#45;xs" icon="cog-sync" />-->
+          <span class="wit-inline-block">Квесты</span>
+        </nuxt-link>
+      </li>
+    </ul>
+
     <div class="wit-flex wit-flex--justify-end wit-flex--align-center wit-block--full-height">
       <ThemeSwitcher />
       <LocaleSwitcher />
@@ -33,11 +68,11 @@ export default {
 </script>
 
 <style scoped>
-    .wit-header {
-        background-color: var(--header-bg);
-        height: 70px;
-        position: sticky;
-        padding: 0 var(--offset-sm);
-        z-index: 50;
-    }
+.wit-header {
+    background-color: var(--header-bg);
+    height: 70px;
+    position: sticky;
+    padding: 0 var(--offset-sm);
+    z-index: 50;
+}
 </style>
---
+++ pages/wishlist.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Settings
+
+    <UserView />
+  </div>
+</template>
+
+<script>
+import UserView from '@/components/UserView'
+
+export default {
+
+    components: {
+        UserView
+    },
+    middleware: ['fetchUser'],
+
+    data: () => ({
+        user: null
+    }),
+
+    created () {
+        this.user = this.$store.state.user
+        // console.log(this.$store)
+
+        // this.$store.commit('add', 'Test')
+
+        // console.log('Created')
+
+        // console.log()
+    },
+
+    methods: {
+
+    }
+}
+</script>
