e918bd6bf86c874d716181ef20315b1c678f0342
Dmitry <v3il@ukr.net>
2020-11-29 21:10:37 +0200

    getCurrentUser update

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,16 @@
-const getCurrentUser = (request, response) => {
-    response.send({ user: request.user })
+import { User } from '../models'
+import { extractUserPublicData } from '../util'
+
+const getCurrentUser = async (request, response) => {
+    if (!request.user) {
+        return response.send({ user: null })
+    }
+
+    const { id } = request.user
+    const user = await User.findOne({ where: { id } })
+    const parsedUser = user ? extractUserPublicData(user) : null
+
+    response.send({ user: parsedUser })
 }
 
 const userController = {
