e50fa473c6e47dd8d2e308814ae34dbb8fc46bc9
Dmitry <v3il@ukr.net>
2020-12-03 23:00:20 +0200

    Quests transaction

--- api/controllers/quests.js
+++ api/controllers/quests.js
@@ -42,81 +42,39 @@ const updateUserQuests = async (request, response) => {
     const canReplaceWeeklyQuests = await witchItApiService.canReplaceWeeklyQuests(accessToken)
     const canReplaceDailyQuests = await witchItApiService.canReplaceDailyQuests(accessToken)
 
-    await Quest.destroy({ where: { userId: user.id } })
-    // await user.removeQuests()
-    // Quest.destroy({ where: { userId: user.id } })
+    const questModels = [...weeklyQuests, ...dailyQuests].map((quest) => {
+        const [rewardId, rewardCount] = quest.rewardVal.split('x')
 
-    // const questsToSave = [...weeklyQuests, ...dailyQuests].map((quest) => {
-    //
-    //     const quest2 = await Quest.create({
-    //         questTask: quest.name,
-    //         questType: quest.type,
-    //         globalId: quest.id,
-    //         localId: quest.questId,
-    //         rewardId,
-    //         rewardCount,
-    //         objective: quest.objective1Max,
-    //         progress: quest.objective1Val
-    //         // user_id: user.id
-    //     })
-    // })
+        return {
+            questTask: quest.name,
+            questType: quest.type,
+            globalId: quest.id,
+            localId: quest.questId,
+            rewardId,
+            rewardCount,
+            objective: quest.objective1Max,
+            progress: quest.objective1Val,
+            userId: user.id
+        }
+    })
 
     let transaction
 
     try {
-        // get transaction
         transaction = await sequelize.transaction()
 
-        for (const quest of [...weeklyQuests, ...dailyQuests]) {
-            const [rewardId, rewardCount] = quest.rewardVal.split('x')
-
-            await Quest.create({
-                questTask: quest.name,
-                questType: quest.type,
-                globalId: quest.id,
-                localId: quest.questId,
-                rewardId,
-                rewardCount,
-                objective: quest.objective1Max,
-                progress: quest.objective1Val,
-                userId: user.id
-            }, { transaction })
-
-            // await user.addQuest(quest2, { transaction })
-        }
+        await Quest.destroy({ where: { userId: user.id } }, { transaction })
+        await Quest.bulkCreate(questModels, { transaction })
 
         await user.update({
             canReplaceWeeklyQuests,
             canReplaceDailyQuests
         }, { transaction })
 
-        // commit
         await transaction.commit()
     } catch (err) {
-        // Rollback transaction only if the transaction object is defined
-        if (transaction) { await transaction.rollback() }
+        await transaction?.rollback()
     }
-
-    // sequelize.transaction(async (transaction) => {
-    //
-    //
-    //     // chain all your queries here. make sure you return them.
-    //     return User.create({
-    //         firstName: 'Abraham',
-    //         lastName: 'Lincoln'
-    //     }, { transaction: t }).then(function (user) {
-    //         return user.setShooter({
-    //             firstName: 'John',
-    //             lastName: 'Boothe'
-    //         }, { transaction: t })
-    //     })
-    // }).then(function (result) {
-    //     // Transaction has been committed
-    //     // result is whatever the result of the promise chain returned to the transaction callback
-    // }).catch(function (err) {
-    //     // Transaction has been rolled back
-    //     // err is whatever rejected the promise chain returned to the transaction callback
-    // })
 
     const userQuests = await user.getQuests()
     const mappedQuests = questsService.mapQuests(userQuests)
