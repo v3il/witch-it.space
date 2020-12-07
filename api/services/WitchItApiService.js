8f570c46689c35c53424a7cdcd49dd5b35a67d60
Dmitry <v3il@ukr.net>
2020-12-07 12:56:38 +0200

    witch it api service refactoring

--- api/services/WitchItApiService.js
+++ api/services/WitchItApiService.js
@@ -1,7 +1,6 @@
 export class WitchItApiService {
     constructor ({ axiosInstance }) {
         this.axiosInstance = axiosInstance
-        // this.questsCollection = questsCollection
     }
 
     async auth (userSteamId) {
@@ -35,15 +34,28 @@ export class WitchItApiService {
         })
 
         const { quests } = data
-
-        // console.log(quests)
-
         const weeklyQuests = quests.filter(quest => quest.type === 'Weekly')
         const dailyQuests = quests.filter(quest => quest.type === 'Daily')
 
         return {
-            weeklyQuests, // .map(this._mapQuest.bind(this)),
-            dailyQuests // .mp(this._mapQuest.bind(this))
+            weeklyQuests,
+            dailyQuests
+        }
+    }
+
+    async loadUserData (steamId) {
+        const authToken = await this.auth(steamId)
+        const accessToken = await this.getAccessToken(authToken)
+        const { weeklyQuests, dailyQuests } = await this.getQuests(accessToken)
+        const canReplaceWeeklyQuests = await this.canReplaceWeeklyQuests(accessToken)
+        const canReplaceDailyQuests = await this.canReplaceDailyQuests(accessToken)
+
+        const quests = [...weeklyQuests, ...dailyQuests]
+
+        return {
+            quests,
+            canReplaceWeeklyQuests,
+            canReplaceDailyQuests
         }
     }
 
@@ -70,49 +82,26 @@ export class WitchItApiService {
     async replaceQuest ({ user, quest }) {
         const authToken = await this.auth(user.steamId)
         const accessToken = await this.getAccessToken(authToken)
+        const isWeeklyQuest = quest.questType === 'Weekly'
 
-        if (quest.questType === 'Weekly') {
-            const { data } = await this.axiosInstance.get('/quest/replaceWeeklyQuest', {
-                params: {
-                    authToken: accessToken,
-                    weeklyQuestId: quest.globalId
-                }
-            })
+        const url = isWeeklyQuest ? '/quest/replaceWeeklyQuest' : '/quest/replaceDailyQuest'
+        const paramName = isWeeklyQuest ? 'weeklyQuestId' : 'dailyQuestId'
 
-            return data.success
-        } else {
-            const { data } = await this.axiosInstance.get('/quest/replaceDailyQuest', {
-                params: {
-                    authToken: accessToken,
-                    dailyQuestId: quest.globalId
-                }
-            })
+        const { data } = await this.axiosInstance.get(url, {
+            params: {
+                authToken: accessToken,
+                [paramName]: quest.globalId
+            }
+        })
 
-            return data.success
-        }
-    }
-
-    async loadUserData (steamId) {
-        const authToken = await this.auth(steamId)
-        const accessToken = await this.getAccessToken(authToken)
-        const { weeklyQuests, dailyQuests } = await this.getQuests(accessToken)
-        const canReplaceWeeklyQuests = await this.canReplaceWeeklyQuests(accessToken)
-        const canReplaceDailyQuests = await this.canReplaceDailyQuests(accessToken)
-
-        const quests = [...weeklyQuests, ...dailyQuests]
-
-        return {
-            quests,
-            canReplaceWeeklyQuests,
-            canReplaceDailyQuests
-        }
+        return data.success
     }
 
     async finalizeQuest ({ user, quest }) {
         const authToken = await this.auth(user.steamId)
         const accessToken = await this.getAccessToken(authToken)
 
-        const r = await this.axiosInstance.get('/quest/finalizeQuest', {
+        const { data } = await this.axiosInstance.get('/quest/finalizeQuest', {
             params: {
                 authToken: accessToken,
                 accountQuestId: quest.globalId,
@@ -120,46 +109,6 @@ export class WitchItApiService {
             }
         })
 
-        return r.data.success
+        return data.success
     }
-
-    // async finalizeQuest({ userId, localQuestId, globalQuestId }) {
-    //     const authToken = await this.auth(userId);
-    //     const accessToken = await this.getAccessToken(authToken);
-    //
-    //     console.log(userId, localQuestId, globalQuestId);
-    //
-    //     const r = await this.axiosInstance.get('/quest/finalizeQuest', {
-    //         params: {
-    //             authToken: accessToken,
-    //             accountQuestId: globalQuestId,
-    //             questId: localQuestId,
-    //         },
-    //     });
-    //
-    //     console.log(r.headers);
-    //
-    //     return r.data.success;
-    // }
-
-    // _mapQuest (quest) {
-    //     const reward = quest.rewardVal
-    //     const [itemId, count] = reward.split('x')
-    //     const rewardItem = items.find(item => item.id === +itemId)
-    //     const progress = Number.parseInt(quest.objective1Val, 10)
-    //     const objective = Number.parseInt(quest.objective1Max, 10)
-    //     const questName = this.questsCollection[quest.name] || `No quest with name "${quest.name}"`
-    //
-    //     delete quest.name
-    //     delete quest.rewardVal
-    //     delete quest.rewardId
-    //     delete quest.objective1Id
-    //     delete quest.objective1Val
-    //     delete quest.objective1Max
-    //     delete quest.type
-    //
-    //     return {
-    //         ...quest, rewardItem, count: +count, progress, objective, questName
-    //     }
-    // }
 }
