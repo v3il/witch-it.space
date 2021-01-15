9cd20a1e4b15de9f46d26de9412ce6ad9cd21dc5
Dmitry <v3il@ukr.net>
2021-01-15 19:35:15 +0200

    Auth Window centering

--- utils/openWindow.js
+++ utils/openWindow.js
@@ -1,6 +1,8 @@
 export const openWindow = (url, options) => {
     const width = Math.min(options.width ?? 800, window.innerWidth)
     const height = Math.min(options.height ?? 800, window.innerHeight)
+    const x = (window.innerWidth - width) / 2
+    const y = (window.innerHeight - height) / 2
 
-    return window.open(url, options.tagName, `width=${width}, height=${height}`)
+    return window.open(url, options.tagName, `width=${width}, height=${height}, left=${x}, top=${y}`)
 }
