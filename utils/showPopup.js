c6d1831fda02970e7049a4c38a3865d934691fc2
Dmitry <v3il@ukr.net>
2021-02-14 19:04:20 +0200

    show popup helper

---
+++ utils/showPopup.js
@@ -0,0 +1,36 @@
+export const showPopup = (vm, options) => {
+    vm.$buefy.dialog.confirm({
+        ...options,
+        animation: 'none',
+        onCancel: () => {
+            const marginRight = getBodyMargin()
+            document.body.style.marginRight = (marginRight - getScrollbarWidth()) + 'px'
+        }
+    })
+
+    const marginRight = getBodyMargin()
+    document.body.style.marginRight = (marginRight + getScrollbarWidth()) + 'px'
+}
+
+function getBodyMargin () {
+    const marginRight = window.getComputedStyle(document.body).marginRight
+    return Number.parseInt(marginRight.match(/\d*/)[0])
+}
+
+function getScrollbarWidth () {
+    const outer = document.createElement('div')
+    outer.style.visibility = 'hidden'
+    outer.style.position = 'absolute'
+    outer.style.left = '-2000px'
+    outer.style.overflow = 'scroll'
+    document.body.appendChild(outer)
+
+    const inner = document.createElement('div')
+    outer.appendChild(inner)
+
+    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth)
+
+    outer.parentNode.removeChild(outer)
+
+    return scrollbarWidth
+}
