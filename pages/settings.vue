cfba1b3071c06e5d2083dc515bdc2154b3a7831d
Dmitry <v3il@ukr.net>
2021-08-15 20:53:06 +0300

    Settings refactoring

--- pages/settings.vue
+++ pages/settings.vue
@@ -19,39 +19,7 @@
     </TopNavBar>
 
     <div class="wit-settings">
-      <!--      <b-message-->
-      <!--        -->
-      <!--        :title="$t('Settings_NotVerifiedAccountTitle')"-->
-      <!--        type="is-danger"-->
-      <!--        :closable="false"-->
-      <!--        class="wis-settings-message wit-line-height&#45;&#45;md wit-offset-bottom&#45;&#45;none"-->
-      <!--        aria-close-label="Close message"-->
-      <!--      >-->
-      <!--        <p class="wit-color&#45;&#45;white wit-offset-bottom&#45;&#45;xs" v-html="$t('Settings_NotVerifiedAccountMessage')" />-->
-
-      <!--        <ul class="wit-color&#45;&#45;warning wit-settings__todo-list">-->
-      <!--          <li v-if="!user.isSteamConnected">-->
-      <!--            {{ $t('Settings_NotVerifiedAccountTask1') }}-->
-      <!--          </li>-->
-      <!--          <li v-if="!user.isDiscordConnected">-->
-      <!--            {{ $t('Settings_NotVerifiedAccountTask2') }}-->
-      <!--          </li>-->
-      <!--          <li v-if="!hasTradeLink">-->
-      <!--            {{ $t('Settings_NotVerifiedAccountTask3') }}-->
-      <!--          </li>-->
-      <!--        </ul>-->
-      <!--      </b-message>-->
-
       <NotVerifiedProfileMessage v-if="!user.isVerified" :profile="user" />
-
-      <!--      <div class="wit-flex wit-flex&#45;&#45;justify-end wit-settings__sticky-panel">-->
-      <!--        <b-field>-->
-      <!--          <b-button type="is-primary" class="wit-offset-left&#45;&#45;auto wit-block" @click="updateSettings">-->
-      <!--            {{ $t('Settings_Save') }}-->
-      <!--          </b-button>-->
-      <!--        </b-field>-->
-      <!--      </div>-->
-
       <StickyPanel @update="updateSettings" />
 
       <template v-if="isAccountMode">
@@ -69,11 +37,8 @@
 
 <script>
 import { mapState } from 'vuex'
-import Card from '@/components/Card'
 import { User } from '@/store'
-import AvatarPicker from '@/components/settings/AvatarPicker'
 import { validateDisplayName, validatePassword, validateSteamTradeURL } from '@/shared/validators'
-import { showPopup } from '@/utils'
 import TopTabs from '@/components/TopTabs'
 import AccountSettings from '@/components/settings/AccountSettings'
 import SocialNetworks from '@/components/settings/SocialNetworks'
@@ -131,18 +96,6 @@ export default {
             User.State.USER
         ]),
 
-        // hasLocalProfile () {
-        //     return this.user.hasLocalProfile
-        // },
-
-        // isProfilePublic () {
-        //     return this.user.isPublic
-        // },
-
-        hasTradeLink () {
-            return !!this.user.steamTradeLink
-        },
-
         isAccountMode () {
             return this.mode === Modes.ACCOUNT
         }
@@ -195,74 +148,6 @@ export default {
                 this.$showError(error)
             }
         }
-
-        // async connectSocial (socialName) {
-        //     try {
-        //         await this.$store.dispatch(User.F.Actions.AUTH_USING_SOCIALS, socialName)
-        //         this.$showSuccess(this.$t('Settings_AccountConnected'))
-        //     } catch (error) {
-        //         this.$showError(error)
-        //     }
-        // },
-        //
-        // disconnectSocial (socialName) {
-        //     showPopup(this, {
-        //         title: this.$t('Settings_DisconnectSocialTitle'),
-        //         message: this.$t('Settings_WannaDisconnectSocial'),
-        //         confirmText: this.$t('Confirm'),
-        //         cancelText: this.$t('Cancel'),
-        //         onConfirm: async () => {
-        //             try {
-        //                 await this.$store.dispatch(User.F.Actions.DISCONNECT_SOCIAL, socialName)
-        //                 this.$showSuccess(this.$t('Settings_AccountDisconnected'))
-        //             } catch (error) {
-        //                 this.$showError(error)
-        //             }
-        //         }
-        //     })
-        // },
-
-        // toggleProfile () {
-        //     this.isProfilePublic ? this.makeProfilePrivate() : this.makeProfilePublic()
-        // },
-        //
-        // async makeProfilePublic () {
-        //     try {
-        //         await this.$store.dispatch(User.F.Actions.TOGGLE_PROFILE, true)
-        //         this.$showSuccess(this.$t('Settings_ProfileVisibilityChanged'))
-        //     } catch (error) {
-        //         this.$showError(error)
-        //     }
-        // },
-        //
-        // makeProfilePrivate () {
-        //     showPopup(this, {
-        //         title: this.$t('Settings_MakePrivatePopupTitle'),
-        //         message: this.$t('Settings_WannaMakePrivate'),
-        //         confirmText: this.$t('Confirm'),
-        //         cancelText: this.$t('Cancel'),
-        //         onConfirm: async () => {
-        //             try {
-        //                 await this.$store.dispatch(User.F.Actions.TOGGLE_PROFILE, false)
-        //                 this.$showSuccess(this.$t('Settings_ProfileVisibilityChanged'))
-        //             } catch (error) {
-        //                 this.$showError(error)
-        //             }
-        //         }
-        //     })
-        // },
-        //
-        // deleteProfile () {
-        //     showPopup(this, {
-        //         title: this.$t('Settings_RemoveProfileTitle'),
-        //         message: this.$t('Settings_WannaRemoveProfile'),
-        //         confirmText: this.$t('Confirm'),
-        //         cancelText: this.$t('Cancel'),
-        //         onConfirm: () => {
-        //             this.$store.dispatch(User.F.Actions.REMOVE_PROFILE).catch(this.$showError)
-        //         }
-        //     })
-        // }
     }
 }
 </script>
