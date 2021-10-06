98dc25499808eeb9f3261e05bc16772a27bbc3e3
Dmitry <v3il@ukr.net>
2021-10-06 19:57:44 +0300

    Removed redundant midlewares

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -1,7 +1,8 @@
 import { Routes } from '@/shared'
+import { User } from '@/store'
 
 export default async function ({ store, redirect }) {
-    await store.dispatch('fetchUser')
+    await store.dispatch(User.Actions.FETCH_USER)
 
     if (!store.state.user) {
         redirect(Routes.LOGIN)
--- middleware/getUserLocale.js
+++
@@ -1,9 +0,0 @@
-// import { Routes } from '@/shared'
-//
-// export default async function ({ store, redirect }) {
-//     await store.dispatch('fetchUser')
-//
-//     if (!store.state.user) {
-//         redirect(Routes.AUTH)
-//     }
-// }
