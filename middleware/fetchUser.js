a7cf146c10e4ef0f3a83d46b609c3bcd1f7a4c86
Dmitry <v3il@ukr.net>
2021-10-15 00:35:09 +0300

    Fetch user optimization

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -2,6 +2,10 @@ import { Routes } from '@/shared'
 import { User } from '@/store'
 
 export default async function ({ store, redirect }) {
+    if (store.state.user.user) {
+        return
+    }
+
     await store.dispatch(User.F.Actions.FETCH_USER)
 
     if (!store.state.user.user) {
