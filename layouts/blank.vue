81435d77281c324cd53cfbd29ef35e86b490ea74
Dmitry <v3il@ukr.net>
2021-10-30 12:00:45 +0300

    new blank layout

---
+++ layouts/blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AuthResult'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/auth/result.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Result
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Status',
+    layout: 'blank'
+}
+</script>
+
+<style scoped>
+
+</style>
