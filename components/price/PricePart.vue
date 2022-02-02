0dd7a04669bc73f432c0f0364ac8d0d35240b873
Dmitry <v3il@ukr.net>
2022-02-03 00:04:38 +0200

    ІусщÑSecondary nts list

--- components/price/PricePart.vue
+++ components/price/PricePart.vue
@@ -11,20 +11,52 @@
       </b-button>
     </template>
 
-    items
+    <div class="wit-paddings--xs">
+      <p class="wit-offset-bottom--xs">
+        Primary ingredients
+      </p>
+
+      <div class="wit-flex wit-offset-bottom--sm">
+        <b-button v-for="item in primaryIngredients" :key="item.id" class="wit-paddings--none wit-offset-right--xs wiz-background--transparent" style="border: 0; width: 45px; height: 45px;">
+          <ItemView :is-title-shown="false" :item="item" type="is-ghost" style="width: 100%;" />
+        </b-button>
+      </div>
+
+      <p class="wit-offset-bottom--xs">
+        Secondary ingredients
+      </p>
+
+      <div class="wit-flex wit-flex--wrap">
+        <b-button v-for="item in secondaryIngredients" :key="item.id" class="wit-paddings--none wit-offset-right--xs wit-offset-bottom--xs wiz-background--transparent" style="border: 0; width: 45px; height: 45px;">
+          <ItemView :is-title-shown="false" :item="item" type="is-ghost" style="width: 100%;" />
+        </b-button>
+      </div>
+    </div>
   </b-dropdown>
 <!--  </div>-->
 </template>
 
 <script>
+import ItemView from '@/components/items/ItemView.vue'
+
 export default {
     name: 'PricePart',
+
+    components: {
+        ItemView
+    },
 
     props: {
         itemId: {
             required: true,
             type: Number
         }
+    },
+
+    created () {
+        this.primaryIngredients = this.$itemsService.getPrimaryIngredients()
+        this.secondaryIngredients = this.$itemsService.getSecondaryIngredients()
+        console.log(this.primaryIngredients)
     }
 }
 </script>
--- domain/services/ItemsService.js
+++ domain/services/ItemsService.js
@@ -13,11 +13,11 @@ export class ItemsService {
         return Object.values(this.#items)
     }
 
-    getPrimaryTradeableItems () {
-        return [901, 902, 903, 904, 905].map(id => this.getById(id))
+    getPrimaryIngredients () {
+        return [900, 901, 902, 903, 904].map(id => this.getById(id))
     }
 
-    getSecondaryTradeableItems () {
-        return [901, 902, 903, 904, 905]
+    getSecondaryIngredients () {
+        return [910, 911, 912, 913, 914, 915, 916, 917, 918].map(id => this.getById(id))
     }
 }
