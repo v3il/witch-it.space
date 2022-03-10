e4d598f36954ad5a490b3964b160dea2d9092a98
Dmitry <v3il@ukr.net>
2022-03-11 00:21:54 +0200

    Migrate user dropdown to dropdown

--- components/header/UserDropdown.vue
+++ components/header/UserDropdown.vue
@@ -1,65 +1,56 @@
 <template>
-  <b-dropdown
-    animation="fade150"
-    class="wit-block--full-height wit-user-dropdown wit-transition--background"
-    :class="dropdownClasses"
-    position="is-bottom-left"
-    @active-change="updateStatus"
-  >
+  <Dropdown position="bottom-end" class="wit-block--full-height">
     <template #trigger>
-      <div class="wit-flex wit-flex--align-center wit-block--full-height">
-        <div class="wit-user-dropdown__image-block wit-offset-right--xs wit-flex wit-flex--center">
+      <b-button type="is-ghost" class="wit-block--full-height wit-user-dropdown wit-transition--background">
+        <div class="wit-flex wit-flex--align-center wit-block--full-height">
           <img
-            class="wit-user-dropdown__image"
+            class="wit-user-dropdown__image wit-offset-right--xs"
             :src="avatarUrl"
             alt="Avatar"
           >
-        </div>
 
-        <div class="username">
-          {{ user.displayName }}
+          <span>{{ user.displayName }}</span>
         </div>
-      </div>
+      </b-button>
     </template>
 
-    <b-dropdown-item class="wit-transition--background">
-      <nuxt-link to="/settings" class="wit-flex wit-flex--align-center wit-color--white">
-        <b-icon size="is-small" class="is-size-5 wit-offset-right--xs" icon="cog-sync" />
-        <span class="wit-inline-block username">{{ $t('Settings') }}</span>
-      </nuxt-link>
-    </b-dropdown-item>
+    <template #items>
+      <DropdownItem>
+        <nuxt-link to="/settings" class="wit-flex wit-flex--align-center wit-color--white">
+          <b-icon size="is-small" class="is-size-5 wit-offset-right--xxs" icon="cog-sync" />
+          <span>{{ $t('Settings') }}</span>
+        </nuxt-link>
+      </DropdownItem>
 
-    <b-dropdown-item class="wit-transition--background" @click="logout">
-      <div class="wit-flex wit-flex--align-center wit-color--danger">
-        <b-icon size="is-small" class="is-size-5 wit-offset-right--xs" icon="logout-variant" />
-        <span class="wit-inline-block">{{ $t('Logout') }}</span>
-      </div>
-    </b-dropdown-item>
-  </b-dropdown>
+      <DropdownItem @click="logout">
+        <div class="wit-flex wit-flex--align-center wit-color--danger">
+          <b-icon size="is-small" class="is-size-5 wit-offset-right--xxs" icon="logout-variant" />
+          <span>{{ $t('Logout') }}</span>
+        </div>
+      </DropdownItem>
+    </template>
+  </Dropdown>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import { User } from '@/store/index.js'
 import { buildAvatarUrl } from '@/utils/index.js'
+import Dropdown from '@/components/basic/dropdown/Dropdown.vue'
+import DropdownItem from '@/components/basic/dropdown/DropdownItem.vue'
 
 export default {
     name: 'UserDropdown',
 
-    data: () => ({
-        isOpen: false
-    }),
+    components: {
+        Dropdown,
+        DropdownItem
+    },
 
     computed: {
         ...mapState(User.PATH, [
             User.State.USER
         ]),
-
-        dropdownClasses () {
-            return {
-                open: this.isOpen
-            }
-        },
 
         avatarUrl () {
             return buildAvatarUrl(this.user.avatarId)
@@ -67,10 +58,6 @@ export default {
     },
 
     methods: {
-        updateStatus (isOpen) {
-            this.isOpen = isOpen
-        },
-
         logout () {
             this.$store.dispatch(User.F.Actions.LOGOUT).catch(this.$showError)
         }
@@ -80,8 +67,6 @@ export default {
 
 <style scoped lang="scss">
 .wit-user-dropdown {
-    --dropdown-item-padding: var(--offset-xs) var(--offset-md);
-
     color: white;
     padding: var(--offset-sm);
     cursor: pointer;
@@ -96,29 +81,10 @@ export default {
     }
 }
 
-.wit-user-dropdown__image-block.with-error {
-    position: relative;
-
-    &::before {
-        content: "";
-        position: absolute;
-        width: 10px;
-        height: 10px;
-        border-radius: 50%;
-        top: -8px;
-        right: -8px;
-        background-color: var(--danger);
-        animation: pulse 10s infinite linear;
-        will-change: opacify;
-        border: 3px solid #36394c;
-        box-sizing: content-box;
-    }
-}
-
 .wit-user-dropdown__image {
-    height: 32px;
-    width: 32px;
+    height: var(--offset-lg);
+    width: var(--offset-lg);
     max-height: none;
-    border-radius: 4px;
+    border-radius: var(--offset-xxs);
 }
 </style>
