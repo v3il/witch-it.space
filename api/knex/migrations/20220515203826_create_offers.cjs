11ac7e72cd59c55292707c51d16e16e506485c27
Dmitry <v3il@ukr.net>
2022-05-15 23:53:00 +0300

    Price model

--- api/knex/migrations/20220515203826_create_offers.cjs
+++ api/knex/migrations/20220515203826_create_offers.cjs
@@ -4,7 +4,7 @@ exports.up = function(knex) {
         table.integer('itemId');
         table.integer('userId').references('id').inTable('users');
         table.timestamp('createdAt').defaultTo(knex.fn.now());
-        table.timestamp('updatedAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').defaultTo(null);
     })
 };
 
