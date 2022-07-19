14a1eae2521ba44c47ba484939b0afa290117e78
Dmitry <v3il@ukr.net>
2022-07-19 18:14:30 +0300

    Split actions into 2 components

---
+++ components/offers/header/ForeignProfileActions.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>1</div>
+</template>
+
+<script>
+export default {
+    name: 'ForeignProfileActions'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/offers/header/OwnProfileActions.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>1</div>
+</template>
+
+<script>
+export default {
+    name: 'OwnProfileActions'
+}
+</script>
+
+<style scoped>
+
+</style>
