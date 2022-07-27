613b29f91c6800956e663af7b0b1f10880adf250
Dmitry <v3il@ukr.net>
2022-07-27 15:08:57 +0300

    Fix offers ssr

--- composables/useOffersPage.js
+++ composables/useOffersPage.js
@@ -1,34 +1,8 @@
-import { computed, ref, useContext, useFetch, useRoute, useStore } from '@nuxtjs/composition-api'
-import { StoreModules } from '@/store/index.js'
-import { ItemsFiltersScheme } from '@/domain/models/schemes/index.js'
-import { Offer } from '@/domain/models/index.js'
+import { computed, ref, useStore } from '@nuxtjs/composition-api'
 import { useStickyUserHeader } from '@/composables/useStickyUserHeader.js'
-import { OfferTypes } from '@/shared/index.js'
 
 export const useOffersPage = (offersType) => {
-    const route = useRoute()
     const store = useStore()
-    const { $wishlistService, $marketService } = useContext()
-    const service = offersType === OfferTypes.MARKET ? $marketService : $wishlistService
-
-    useFetch(async () => {
-        await store.dispatch(`${StoreModules.OFFERS}/setOffersType`, offersType)
-
-        await store.dispatch(`${StoreModules.FILTERS}/setData`, {
-            defaultFilters: ItemsFiltersScheme.getDefaultFilters(),
-            defaultSorts: ItemsFiltersScheme.getDefaultSorts(),
-            availableSorts: ItemsFiltersScheme.getAvailableSorts()
-        })
-
-        const { offers, profile } = await service.fetch(route.value.params.id)
-
-        await store.dispatch(`${StoreModules.OFFERS}/setProfile`, profile)
-        await store.dispatch(`${StoreModules.OFFERS}/storeOffers`, {
-            existingOffers: offers.map(offer => Offer.create(offer)),
-            availableOffers: []
-        })
-    })
-
     const filters = computed(() => store.state.filters.filters)
     const sortedOffers = computed(() => store.getters['offers/sortedOfferModels'])
 
--- pages/profiles/_id/wishlist/index.vue
+++ pages/profiles/_id/wishlist/index.vue
@@ -26,7 +26,10 @@
 import { SidebarPanel } from '@/components/basic'
 import { OffersEmptyState, OffersFilter, OffersList, OfferTabs, Search, UserHeader } from '@/components/offers'
 import { useOffersPage } from '@/composables'
-import { OfferTypes } from '@/shared/index.js'
+import { OfferTypes, Routes } from '@/shared/index.js'
+import { StoreModules } from '@/store/index.js'
+import { ItemsFiltersScheme } from '@/domain/models/schemes/index.js'
+import { Offer } from '@/domain/models/index.js'
 
 export default {
     components: {
@@ -51,6 +54,29 @@ export default {
         } = useOffersPage(OfferTypes.WISHLIST)
 
         return { offersType, isFiltersVisible, isStickyHeaderVisible, filters, sortedOffers }
+    },
+
+    // TODO: use Composition API
+    async asyncData ({ store, route, $wishlistService, error, $t }) {
+        await store.dispatch(`${StoreModules.OFFERS}/setOffersType`, OfferTypes.WISHLIST)
+
+        await store.dispatch(`${StoreModules.FILTERS}/setData`, {
+            defaultFilters: ItemsFiltersScheme.getDefaultFilters(),
+            defaultSorts: ItemsFiltersScheme.getDefaultSorts(),
+            availableSorts: ItemsFiltersScheme.getAvailableSorts()
+        })
+
+        const { offers, profile } = await $wishlistService.fetch(route.params.id)
+
+        if (!profile) {
+            return error({ statusCode: 404, linkTitle: $t('Profiles_BackToProfilesList'), linkUrl: Routes.PROFILES })
+        }
+
+        await store.dispatch(`${StoreModules.OFFERS}/setProfile`, profile)
+        await store.dispatch(`${StoreModules.OFFERS}/storeOffers`, {
+            existingOffers: offers.map(offer => Offer.create(offer)),
+            availableOffers: []
+        })
     }
 }
 </script>
