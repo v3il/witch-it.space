2be8fc144a07f52dd44abf535686e984db82a625
Dmitry <v3il@ukr.net>
2022-08-30 22:26:53 +0300

    Settings tab

--- components/settings/tabs/SettingsTabs.vue
+++ components/settings/tabs/SettingsTabs.vue
@@ -6,9 +6,13 @@
       </h1>
     </div>
 
-    <ul>
-      <SettingsTabsItem v-for="tabData in tabsData" :key="tabData.id" :tab-data="tabData" :current-tab="currentTab" />
-    </ul>
+    <SettingsTabsItem
+      v-for="tabData in tabsData"
+      :key="tabData.id"
+      :tab-data="tabData"
+      :current-tab="currentTab"
+      @switch="currentTab = $event"
+    />
   </div>
 </template>
 
@@ -25,10 +29,11 @@ export default {
         const currentTab = ref('account')
 
         const tabsData = [
-            { id: 'account', title: 'Settings', description: '123', icon: 'account-circle-outline' },
-            { id: 'account2', title: 'Settings2', description: '123', icon: 'account-circle-outline' },
-            { id: 'account3', title: 'Settings3', description: '123', icon: 'account-circle-outline' },
-            { id: 'account4', title: 'Settings4', description: '123', icon: 'account-circle-outline' }
+            { id: 'account', title: 'Settings_Tabs_Account_Title', description: 'Settings_Tabs_Account_Description', icon: 'account-circle-outline' },
+            { id: 'security', title: 'Settings_Tabs_Security_Title', description: 'Settings_Tabs_Security_Description', icon: 'lock-outline' },
+            { id: 'steam', title: 'Settings_Tabs_Steam_Title', description: 'Settings_Tabs_Steam_Description', icon: 'steam' },
+            { id: 'market', title: 'Settings_Tabs_Market_Title', description: 'Settings_Tabs_Market_Description', icon: 'shopping-outline' },
+            { id: 'danger', title: 'Settings_Tabs_AccountActions_Title', description: 'Settings_Tabs_AccountActions_Description', icon: 'alert-circle-outline' }
         ]
 
         return { tabsData, currentTab }
@@ -56,7 +61,7 @@ $border-color: rgba(241, 245, 249, 0.12);
 .wis-settings-tabs__title {
     letter-spacing: -0.025em;
     line-height: 1;
-    font-weight: 800;
+    font-weight: 600;
     font-size: 2rem;
 }
 
--- components/settings/tabs/SettingsTabsItem.vue
+++ components/settings/tabs/SettingsTabsItem.vue
@@ -1,19 +1,17 @@
 <template>
-  <li>
-    <div role="button" class="wit-flex wis-settings-tab" :class="tabClasses">
-      <i class="mdi mdi-24px wit-color--muted wis-settings-tab__icon" :class="iconClass" />
+  <div role="button" class="wit-flex wis-settings-tab" :class="tabClasses" @click="onSwitch">
+    <i class="mdi mdi-24px wit-color--muted wis-settings-tab__icon" :class="iconClass" />
 
-      <div>
-        <h2 class="wis-settings-tabs__title">
-          Account
-        </h2>
+    <div>
+      <h2 class="wis-settings-tabs__title">
+        {{ $t(tabData.title) }}
+      </h2>
 
-        <p class="wit-color--muted wis-settings-tabs__description">
-          Manage your public profile and private information
-        </p>
-      </div>
+      <p class="wit-color--muted wis-settings-tabs__description">
+        {{ $t(tabData.description) }}
+      </p>
     </div>
-  </li>
+  </div>
 </template>
 
 <script>
@@ -34,12 +32,13 @@ export default {
         }
     },
 
-    setup (props) {
+    setup (props, { emit }) {
         const iconClass = computed(() => `mdi-${props.tabData.icon}`)
         const isActive = computed(() => props.currentTab === props.tabData.id)
         const tabClasses = computed(() => ({ active: isActive.value }))
+        const onSwitch = () => emit('switch', props.tabData.id)
 
-        return { iconClass, tabClasses }
+        return { iconClass, tabClasses, onSwitch }
     }
 }
 </script>
@@ -48,6 +47,9 @@ export default {
 $border-color: rgba(241, 245, 249, 0.12);
 
 .wis-settings-tab {
+    --title-color: #fff;
+    --icon-color: rgb(148, 163, 184);
+
     height: auto;
     padding: 20px 32px;
     border-bottom: 1px solid $border-color;
@@ -57,16 +59,23 @@ $border-color: rgba(241, 245, 249, 0.12);
     &.active {
         background-color: rgba(255, 255, 255, 0.05);
     }
+
+    &.active {
+        --title-color: rgb(99, 102, 241);
+        --icon-color: rgb(99, 102, 241);
+    }
 }
 
 .wis-settings-tabs__title {
     line-height: 1.5rem;
     font-weight: 500;
     margin-bottom: 2px;
+    color: var(--title-color);
 }
 
 .wis-settings-tab__icon {
     margin-right: 12px;
+    color: var(--icon-color);
 }
 
 .wis-settings-tabs__description {
