af8ad2db7bd6b7055cad87b2eddffc482f4c4261
Dmitry <v3il@ukr.net>
2022-10-23 01:45:09 +0300

    Update price editor

---
+++ components/price/IngredientsGrid.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wis-ingredients-grid">
+    <b-button
+      v-for="item in ingredients"
+      :key="item.id"
+      :disabled="item.id === selectedItemId"
+      class="wit-paddings--none wis-ingredients-grid__button"
+      @click="$emit('select', item)"
+    >
+      <ItemView add-tooltip :is-title-shown="false" :item="item" type="is-ghost" />
+    </b-button>
+  </div>
+</template>
+
+<script>
+import ItemView from '../items/ItemView.vue'
+
+export default {
+    name: 'IngredientsGrid',
+
+    components: {
+        ItemView
+    },
+
+    props: {
+        ingredients: {
+            required: true,
+            type: Array
+        },
+
+        selectedItemId: {
+            required: true,
+            type: Number
+        }
+    }
+}
+</script>
+
+<style scoped>
+.wis-ingredients-grid {
+    display: grid;
+    grid-template-columns: repeat(5, 45px);
+    gap: 8px;
+}
+
+.wis-ingredients-grid__button {
+    border: 0;
+    width: 40px;
+    height: 40px;
+}
+</style>
--- components/price/PricePart.vue
+++ components/price/PricePart.vue
@@ -3,51 +3,35 @@
     <v-popover ref="popover" :placement="popoverPosition">
       <b-button
         type="is-link"
-        class="wit-position--relative"
-        style="width: 48px; height: 48px; background: transparent; border: 1px dashed #626ed4;"
-        @click="show1"
+        class="wit-position--relative wis-price-place"
+        @click="showPopover"
       >
-        <template v-if="selectedItem">
-          <ItemView :item="selectedItem" :is-title-shown="false" style="width: 45px;" />
-        </template>
-
+        <ItemView v-if="selectedItem" :item="selectedItem" :is-title-shown="false" style="width: 45px;" />
         <i v-else class="mdi mdi-24 mdi-plus" />
       </b-button>
 
-      <div v-if="show" slot="popover" class="wit-paddings--xs">
+      <div v-if="isVisible" slot="popover" class="wit-paddings--xs">
         <p class="wit-offset-bottom--xs">
           Primary ingredients
         </p>
 
-        <div class="wit-flex wit-offset-bottom--sm" style="display: grid; grid-template-columns: repeat(5, 45px); column-gap: 8px;">
-          <b-button
-            v-for="item in primaryIngredients"
-            :key="item.id"
-            :disabled="item.id === pairedItemId"
-            class="wit-paddings--none wit-offset-right--xs wiz-background--transparent wit-flex__item--no-shrink"
-            style="border: 0; width: 40px; height: 40px;"
-            @click="onItemSelect(item)"
-          >
-            <ItemView add-tooltip :is-title-shown="false" :item="item" type="is-ghost" style="width: 100%;" />
-          </b-button>
-        </div>
+        <IngredientsGrid
+          :ingredients="primaryIngredients"
+          :selected-item-id="pairedItemId"
+          class="wit-offset-bottom--sm"
+          @select="onItemSelect"
+        />
 
         <p class="wit-offset-bottom--xs">
           Secondary ingredients
         </p>
 
-        <div class="wit-flex wit-flex--wrap wit-offset-bottom--xs" style="display: grid; grid-template-columns: repeat(5, 45px); column-gap: 8px;">
-          <b-button
-            v-for="item in secondaryIngredients"
-            :key="item.id"
-            :disabled="item.id === pairedItemId"
-            class="wit-flex__item--no-shrink wit-paddings--none wit-offset-right--xs wit-offset-bottom--xs wiz-background--transparent"
-            style="border: 0; width: 40px; height: 40px;"
-            @click="onItemSelect(item)"
-          >
-            <ItemView add-tooltip :is-title-shown="false" :item="item" type="is-ghost" style="width: 100%;" />
-          </b-button>
-        </div>
+        <IngredientsGrid
+          :ingredients="secondaryIngredients"
+          :selected-item-id="pairedItemId"
+          class="wit-offset-bottom--sm"
+          @select="onItemSelect"
+        />
 
         <b-button type="is-danger" expanded @click="onItemClear">
           Remove
@@ -58,6 +42,8 @@
 </template>
 
 <script>
+import { ref, useContext, computed } from '@nuxtjs/composition-api'
+import IngredientsGrid from './IngredientsGrid.vue'
 import ItemView from '@/components/items/ItemView.vue'
 import RemoveButton from '~/components/basic/RemoveButton.vue'
 
@@ -66,7 +52,8 @@ export default {
 
     components: {
         ItemView,
-        RemoveButton
+        RemoveButton,
+        IngredientsGrid
     },
 
     props: {
@@ -87,36 +74,50 @@ export default {
         }
     },
 
-    data: () => ({
-        show: false
-    }),
+    setup (props, { emit }) {
+        const { $itemsService } = useContext()
 
-    computed: {
-        selectedItem () {
-            return this.$itemsService.getById(this.itemId)
+        const isVisible = ref(false)
+        const popover = ref(null)
+
+        console.error(popover)
+
+        const selectedItem = computed(() => $itemsService.getById(props.itemId))
+
+        const primaryIngredients = $itemsService.getPrimaryIngredients()
+        const secondaryIngredients = $itemsService.getSecondaryIngredients()
+
+        const showPopover = () => isVisible.value = true
+
+        const onItemSelect = (item) => {
+            emit('itemSelect', item.id)
+            popover.value.hide()
         }
-    },
 
-    created () {
-        this.primaryIngredients = this.$itemsService.getPrimaryIngredients()
-        this.secondaryIngredients = this.$itemsService.getSecondaryIngredients()
-    },
+        const onItemClear = () => {
+            emit('itemClear')
+            popover.value.hide()
+        }
 
-    methods: {
-        show1 () {
-            this.show = true
-            // this.$refs.popover.open()
-        },
-
-        onItemSelect (item) {
-            this.$emit('itemSelect', item.id)
-            this.$refs.popover.hide()
-        },
-
-        onItemClear () {
-            this.$emit('itemClear')
-            this.$refs.popover.hide()
+        return {
+            popover,
+            isVisible,
+            primaryIngredients,
+            secondaryIngredients,
+            selectedItem,
+            onItemSelect,
+            onItemClear,
+            showPopover
         }
     }
 }
 </script>
+
+<style scoped lang="scss">
+.wis-price-place {
+    width: 48px;
+    height: 48px;
+    background: transparent;
+    border: 1px dashed #626ed4;
+}
+</style>
